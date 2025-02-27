1b1ddd4b370daa076ba7e46845f0475e6f1ee413
Minh Tuyen Nguyen <minh@nguyen365.com>
2025-02-27 14:59:01 -0500

    Added projects

---
+++ WebApplication/Scripts/operation.js
@@ -0,0 +1,5 @@
+﻿function add(a, b) {
+    return a + b;
+}
+
+module.exports = add;
---
+++ WebApplication/Scripts/tests/operation.test.js
@@ -0,0 +1,5 @@
+﻿const add = require('../operation');
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(add(1, 2)).toBe(3);
+});
