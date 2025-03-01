e4e5ea588d872bba4a245fb15e39f13ee12ebb39
Minh Tuyen Nguyen <minh@nguyen365.com>
2025-03-01 06:00:00 -0500

    obtain correct path to OpenCover and ReportGenerator

---
+++ WebApplication.Test/PersonTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WebApplication.Test
+{
+    [TestClass]
+    public sealed class PersonTests : Person
+    {
+        [TestMethod]
+        public void FirstName_ShouldBeSetAndRetrievedCorrectly()
+        {
+            // Arrange
+            var person = new Person
+            {
+                // Act
+                FirstName = "John"
+            };
+
+            // Assert
+            Assert.AreEqual("John", person.FirstName);
+        }
+
+        [TestMethod]
+        public void LastName_ShouldBeSetAndRetrievedCorrectly()
+        {
+            // Arrange
+            var person = new Person
+            {
+                // Act
+                LastName = "Doe"
+            };
+
+            // Assert
+            Assert.AreEqual("Doe", person.LastName);
+        }
+
+        [TestMethod]
+        public void Person_ShouldAllowEmptyFirstName()
+        {
+            // Arrange
+            var person = new Person
+            {
+                // Act
+                FirstName = ""
+            };
+
+            // Assert
+            Assert.AreEqual("", person.FirstName);
+        }
+
+        [TestMethod]
+        public void Person_ShouldAllowEmptyLastName()
+        {
+            // Arrange
+            var person = new Person
+            {
+                // Act
+                LastName = ""
+            };
+
+            // Assert
+            Assert.AreEqual("", person.LastName);
+        }
+
+        [TestMethod]
+        public void Person_ShouldInitializeWithDefaultValues()
+        {
+            // Act
+            var person = new Person();
+
+            // Assert
+            Assert.IsNull(person.FirstName);
+            Assert.IsNull(person.LastName);
+        }
+    }
+}
